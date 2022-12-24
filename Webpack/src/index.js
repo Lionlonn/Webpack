cbbae2d0b6ac8e325a27b6f0c8a8fabb847c784e
Lionlonn <113461939+Lionlonn@users.noreply.github.com>
2022-12-24 14:28:28 +0300

    webpack
    webpack

---
+++ Webpack/src/index.js
@@ -0,0 +1,14 @@
+
+import './index.html';
+import './index.scss';
+import menu3 from './img/menu-3.svg'
+import { mult, sum } from './modules/calc';
+
+const img = new Image();
+img.src = menu-3;
+
+
+
+console.log(mult(5, 4));
+console.log(sum(5, 4));
+
--- webpacktemplate/src/index.js
+++
@@ -1,6 +0,0 @@
-import './js/common'
-
-// import 'vue'
-// import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
-// import Vue from 'vue'
-window.Vue = require('vue')
